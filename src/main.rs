3806227c6da650d65d22962631d9dd771ce12a1b
ukaushik-98 <kaushikuk101@gmail.com>
2026-03-04 20:26:58 -0800

    init routes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use axum::{Router, extract::Path, routing::get};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/greet/{name}", get(greet));
+    let listener = TcpListener::bind("0.0.0.0:3000").await?;
+    let _ = axum::serve(listener, app).await;
+    Ok(())
+}
+
+async fn root() -> &'static str {
+    "Hello World!"
+}
+
+async fn greet(Path(name): Path<String>) -> String {
+    format!("Hello {}", name)
 }
